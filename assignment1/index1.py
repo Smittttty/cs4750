aa80bbc67d6c63cc83b6cfc47683e00adc2cea12
Adam Smith <aes702@mun.ca>
2016-09-22 20:21:19 -0230

    Added comments

--- assignment1/index1.py
+++ assignment1/index1.py
@@ -10,46 +10,81 @@ import sys
 import string
 import collections
 
+#########################
+## loadIgnoreList(fileName)
+## Input: fileName: path to file.
+## return: return set of words from specified file
+#########################
 def loadIgnoreList(fileName):
+	#open file
 	f = open(fileName, "r")
+	#initalize set
 	words = set()
-	
+
+	#read each word in file
 	for line in f:
-		for word in line.split():
-			words.add(word)
-	
+		#add word to set
+		words.add(word)
+
+	#close file
 	f.close()
-	
+
+	#return set
 	return words
-	
+
+#########################
+## getLineNumbers(ignored, fileName)
+## Input: ignored: set containing words to ignore
+## fileName: path to file to generate line numbers from
+## return: disctionary with word => line numbers
+#########################
 def getLineNumbers(ignored, fileName):
 	lines = {}
+	#open file for reading
 	f = open(fileName, "r")
+
+	#keep track of line number
 	lineNumber = 1
+	#iterate through lines
 	for line in f:
+		#split each word in line
 		for word in line.split():
+			#remove punctuation
 			word = word.translate(None, string.punctuation)
+			#check valid word length
 			if not len(word) > 0:
 				continue
+			#check if word is not in ignored list
 			if not word in ignored:
+				#append lineNumber to value of list[word]
 				if not lines.has_key(word):
 					lines[word] = str(lineNumber) + " "
 				else:
 					lines[word] += str(lineNumber) + " "
+		#increment line number
 		lineNumber += 1
-			
+
+	#return lines
 	return lines
 
+#ignored path
 ignored = sys.argv[1]
+#text path
 text = sys.argv[2]
+#output path
 output = sys.argv[3]
-	
+
+#generate line numbers
 lines = getLineNumbers(loadIgnoreList(ignored), text)
+#sort the array by key
 sortedLines = collections.OrderedDict(sorted(lines.items()))
 
+#open to write
 f = open(output, "w")
 
+#write key: lineNumbers
 for key, value in sortedLines.items():
-	f.write(key + ": " + value + "\r\n")
+	f.write(key + ": " + value + "\n")
 
+#close the file!
 f.close()
--- assignment1/tcomp2.py
+++ assignment1/tcomp2.py
@@ -9,43 +9,78 @@
 from __future__ import division
 import sys
 
-file1 = "tc1.dat"
-file2 = "tc2.dat"
-
+#########################
+## nW(fileName)
+## Input: fileName: file to generate unique words from
+## return: set of unique words
+#########################
 def nW(fileName):
+	#open file
 	f = open(fileName, "r")
 	words = set()
+	#iterate throug each line
 	for line in f:
+		#split line into words
 		for word in line.split():
+			#if word not already in set, add to set
 			if not word in words:
 				words.add(word)
-				
+
+	#close file
 	f.close()
+
+	#return set
 	return words
 
+#########################
+## SD(x, y)
+## Input: x: word set 1
+## y: word set 2
+## return: return set difference
+#########################
 def SD(x, y):
+	#count difference of x from y
 	count = len(x.difference(y))
-	count += len(y.difference(x))		
-	
+	#count difference of y from x
+	count += len(y.difference(x))
+
+	#return difference
 	return count
-	
+
+#########################
+## SD(x, y)
+## Input: x: file1 to compare
+## y: file 2 to compare
+## return: return score between two files
+#########################
 def sim(x, y):
+	#get set from file x
 	words1 = nW(x);
+	#get set from file y
 	words2 = nW(y);
-	
+	# calculate and return difference
 	return 1.0 - (SD(words1, words2) / (len(words1) + len(words2)))
-	
+
+#get arg count
 argc = len(sys.argv)
-file1 = sys.argv[1]	
+#get inital compare file
+file1 = sys.argv[1]
+
+#setup to find best file
 best = -1;
 bestname = "none"
 
+#loop through each arg
 for i in range(2, argc):
+	#compate files
 	tmp = round(sim(file1, sys.argv[i]), 3)
 	tmp += 0
+	#print result
 	print "Sim(\"" + file1 + "\", \"" + sys.argv[i] + "\") = {:.3f}".format(tmp)
+	#set best if it is
 	if tmp >= best:
 		best=tmp
 		bestname = sys.argv[i]
 
+#print best file
 print "File " + "\"" + file1 + "\" is most similar to file \"" + bestname + "\"."
